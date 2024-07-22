67f1f4897fb57b15a3b6a27a5006c754f2bf9a47
Petr Tesar <petr.tesar@skf.com>
2024-07-23 00:49:42 +0200

    fence I

--- public/src/components/FenceI.component.tsx
+++ public/src/components/FenceI.component.tsx
@@ -11,12 +11,12 @@ const FenceI = (props: {
 
     const { useState, useEffect, useRef } = React;
 
-    const [fenceLength, setFenceLength] = useState(0);
-    const [fenceWidth, setFenceWidth] = useState(0);
+    const [fenceHeight, setFenceHeightOper] = useState(0);
+    const [fenceWidth, setFenceWidthOper] = useState(0);
     //const canvasRef = useRef(null);
 
     useEffect(() => {
-        if (fenceLength > 0 && fenceWidth > 0) {
+        if (fenceHeight > 0 && fenceWidth > 0) {
             //const canvas = canvasRef.current;
             //const ctx = canvas.getContext('2d');
             //ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -30,16 +30,32 @@ const FenceI = (props: {
             //ctx.fillRect(10 + 10, 10, 10, 10); // Starting post
             //ctx.fillRect(10 + fenceLength * 10, 10, 10, 10); // Ending post
         }
-    }, [fenceLength, fenceWidth]);
+    }, [fenceHeight, fenceWidth]);
 
-    const handleLengthChange = (e) => {
-        const length = e.target.value;
-        setFenceLength(length);
+    const handleHeightChange = (e) => {
+        const height = e.target.value;
+        setFenceHeightOper(height);
+    };
+
+    const heightChangePlus = () => {
+        setFenceHeightOper(fenceHeight + 0.1);
+    };
+
+    const heightChangeMinus = () => {
+        setFenceHeightOper(fenceHeight - 0.1);
     };
 
     const handleWidthChange = (e) => {
         const width = e.target.value;
-        setFenceWidth(width);
+        setFenceWidthOper(width);
+    };
+
+    const widthChangePlus = () => {
+        setFenceWidthOper(fenceWidth + 0.1);
+    };
+
+    const widthChangeMinus = () => {
+        setFenceWidthOper(fenceWidth - 0.1);
     };
 
     const calculatePrice = () => {
@@ -48,15 +64,15 @@ const FenceI = (props: {
         let panelCountTotal = 0;
         let totalPostCount = 0;
 
-        if (fenceLength > 0 && fenceWidth > 0) {
+        if (fenceHeight > 0 && fenceWidth > 0) {
             let horizontalPanelCount;
             let verticalPanelCount;
             let postCount;
 
             const heightFence = Math.ceil(heightValue / meter);
 
-            horizontalPanelCount = Math.ceil((fenceLength / (width / meter)) * fenceWidth); // Assuming each panel width in m
-            postCount = Math.ceil((fenceLength / (width / meter)) + 1); // Assuming one post for each end of the panel
+            horizontalPanelCount = Math.ceil((fenceHeight / (width / meter)) * fenceWidth); // Assuming each panel width in m
+            postCount = Math.ceil((fenceHeight / (width / meter)) + 1); // Assuming one post for each end of the panel
             verticalPanelCount = horizontalPanelCount * heightFence;
 
             const panelPrice = (horizontalPanelCount + (verticalPanelCount || 0)) * priceValue;
@@ -84,7 +100,7 @@ const FenceI = (props: {
             <div>
                 <label htmlFor="lengthInput" className="block mb-2 text-sm font-medium text-gray-900">Zadejte délku strany (v m):</label>
                 <div className="relative flex items-center max-w-[11rem]">
-                    <button type="button" id="decrement-button" data-input-counter-decrement="lengthInput" className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
+                    <button type="button" onClick={widthChangeMinus} id="decrement-button" data-input-counter-decrement="lengthInput" className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
                         <svg className="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h16" />
                         </svg>
@@ -92,22 +108,19 @@ const FenceI = (props: {
                     <input
                         type="number"
                         id="lengthInput"
-                        value={fenceLength}
-                        onChange={handleLengthChange}
-                        step="0.1"
-                        min="1"
-                        max="50"
-                        aria-describedby="helper-text-explanation"
+                        value={fenceWidth}
+                        onChange={handleWidthChange}
                         className="bg-gray-50 border-x-0 border-gray-300 h-11 font-medium text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full pb-6 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder=""
-                        required />
+                        required 
+                        readOnly
+                        />
                     <div className="absolute bottom-1 start-1/2 -translate-x-1/2 rtl:translate-x-1/2 flex items-center text-xs text-gray-400 space-x-1 rtl:space-x-reverse">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-3 h-3">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
                         </svg>
                         <span>délka plotu</span>
                     </div>
-                    <button type="button" id="increment-button" data-input-counter-increment="lengthInput" className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
+                    <button type="button" onClick={widthChangePlus} id="increment-button" data-input-counter-increment="lengthInput" className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
                         <svg className="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
                         </svg>
@@ -119,32 +132,27 @@ const FenceI = (props: {
             <div>
                 <label htmlFor="quantity-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Zadejte výšku strany (v m):</label>
                 <div className="relative flex items-center max-w-[11rem]">
-                    <button type="button" id="decrement-button" data-input-counter-decrement="quantity-input" className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
+                    <button type="button" onClick={heightChangeMinus} id="decrement-button" data-input-counter-decrement="quantity-input" className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
                         <svg className="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h16" />
                         </svg>
                     </button>
                     <input
-                    
                         type="number"
                         id="quantity-input"
-                        value={fenceWidth}
-                        onChange={handleWidthChange}
-                        step="0.1"
-                        min="1"
-                        max="3"
-                        data-input-counter
-                        aria-describedby="helper-text-explanation"
+                        value={fenceHeight}
+                        onChange={handleHeightChange}
                         className="bg-gray-50 border-x-0 border-gray-300 h-11 font-medium text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full pb-6 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder=""
-                        required />
+                        required
+                        readOnly
+                        />
                     <div className="absolute bottom-1 start-1/2 -translate-x-1/2 rtl:translate-x-1/2 flex items-center text-xs text-gray-400 space-x-1 rtl:space-x-reverse">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-3 h-3">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M3 4.5h14.25M3 9h9.75M3 13.5h5.25m5.25-.75L17.25 9m0 0L21 12.75M17.25 9v12" />
                         </svg>
                         <span>výška plotu</span>
                     </div>
-                    <button type="button" id="increment-button" data-input-counter-increment="quantity-input" className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
+                    <button type="button" onClick={heightChangePlus}  id="increment-button" data-input-counter-increment="quantity-input" className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
                         <svg className="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
                         </svg>
@@ -166,8 +174,8 @@ const FenceI = (props: {
                             <input type="hidden" id="totalPostCount" name="totalPostCount" value={totalPostCount} />
                         </li>
                         <li>
-                            <span className="text-sm font-medium text-gray-900">Délka plotu:  {fenceLength} m </span>
-                            <input type="hidden" id="fenceLength" name="fenceLength" value={fenceLength} />
+                            <span className="text-sm font-medium text-gray-900">Délka plotu:  {fenceHeight} m </span>
+                            <input type="hidden" id="fenceHeight" name="fenceHeight" value={fenceHeight} />
                         </li>
                         <li>
                             <span className="text-sm font-medium text-gray-900">Výška sloupku a plotu:  {fenceWidth} m </span>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -68,15 +68,32 @@
                   </li>
                 </ul>
               </div>
-              <div class="flex items-center justify-between">
+              <div class="md:hidden flex items-center">
+                <button class="mobile-menu-button">
+                    <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                    </svg>
+                </button>
+            </div>
+              <!--
+                <div class="flex items-center justify-between">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" class="w-5 h-5">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
                 </svg>
     
                 <a href="#" class="block ml-1 py-2 px-3 font-medium text-white md:p-0">Katalog 2024</a>
     
-              </div>
+                </div>
+              -->
+              <!-- mobile menu -->
+              <div class="mobile-menu hidden md:hidden">
+                <a href="" class="block py-2 px-4 text-sm hover:bg-gray-200">Home</a>
+                <a href="" class="block py-2 px-4 text-sm hover:bg-gray-200">About</a>
+                <a href="" class="block py-2 px-4 text-sm hover:bg-gray-200">Contact</a>
             </div>
+            </div>
+            
+            
           </nav>
     </header>
     <main>
@@ -119,5 +136,15 @@
             </div>
           </div>
     </footer>
+
+    <script>
+      const btn = document.querySelector('button.mobile-menu-button');
+      const menu = document.querySelector('.mobile-menu');
+
+      btn.addEventListener('click', () => {
+          menu.classList.toggle('hidden');
+      });
+  </script>
+  
 </body>
 </html>
